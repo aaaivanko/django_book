24ffae988a782db89ce9e7a576515f39a316444c
Ivan <ivan.chvartatskyy@gmail.com>
2022-07-18 17:33:59 +0200

    added sceleton to simple vosualization

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -9,3 +9,20 @@ class Topic(models.Model):
     def __str__(self):
         """Return string description of model"""
         return self.text
+
+
+class Entry(models.Model):
+    """Some concrete information about some topic."""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Return model in str."""
+        if len(self.text) < 50:
+            return f"{self.text}"
+        else:
+            return f"{self.text[:50]}...."
